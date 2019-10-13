5ba63c229ea2ab9616691fb3b17123739e8612b4
Osama Abu Sitta <o.abusitta@Tahaluf.ae>
2019-10-13 10:31:05 +0400

    Deployment fixes

--- AngularVSExtension/VSPackage.cs
+++ AngularVSExtension/VSPackage.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.Shell;
+﻿using EnvDTE80;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -10,9 +12,9 @@ namespace AngularVSExtension
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.guidTypeScriptHtmlSwitchCommandPackageString)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideService(typeof(Microsoft.VisualStudio.Shell.Interop.SDTE), IsAsyncQueryable = true)]
-    //[ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
-    //[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
     //[ProvideAutoLoad("f1536ef8-92ec-443c-9ed7-fdadf150da82")]
     //[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     public sealed class VSPackage : AsyncPackage
